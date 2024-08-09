2ddcc63865b56ff711c9d0dab4cc39ffcc8ef935
Shubham13112004 <158588207+Shubham13112004@users.noreply.github.com>
2024-08-10 00:47:45 +0530

    Add files via upload

---
+++ area_calculate.py
@@ -0,0 +1,7 @@
+a=float(input("enter the radius"))
+print("area of circle is:",format(3.14*a*a,".2f"))
+
+a=float(input("enter the lenght"))
+b=float(input("enter the breadth"))
+print("area of rectangke is:",format(0.5*a*b,".2f"))
+
