0c4b62a4c952da525fe74e35aba264e511f6cd54
Shubham13112004 <158588207+Shubham13112004@users.noreply.github.com>
2024-08-10 00:48:25 +0530

    Add files via upload

---
+++ implicit_type_coversion.py
@@ -0,0 +1,12 @@
+a=int(input("enter a digit"))
+print(type(a))
+print(float(a))
+print(complex(a))
+
+a=float(input("enter a digit"))
+print(type(a))
+print(int(a))
+print(complex(a))
+
+a=complex(input("enter a digit"))
+print(type(a))
