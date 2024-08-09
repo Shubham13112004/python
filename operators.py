15160cfb04d6bc06f9dc0142c7677832ce61b2b0
Shubham13112004 <158588207+Shubham13112004@users.noreply.github.com>
2024-08-10 00:48:43 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,25 @@
+a=float(input("enter the 1st digit"))
+b=float(input("enter the 2nd digit"))
+
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a//b
+
+ans=input("enter")
+if ans=='c':
+    print(c)
+    print(f"addition is {a}+{b}={c}")
+
+elif ans=='d':
+    print(d)
+
+elif ans=='e':
+    print(e)
+
+elif ans=='f':
+    print(f)
+
+elif ans=='g':
+    print(g)
